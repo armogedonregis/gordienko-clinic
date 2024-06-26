0ba6896825649b4282784d9dad917532f607fa47
denosaur98 <denischurchill1998@gmail.com>
2024-06-26 22:09:36 +0300

    readresults done

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,39 +1,40 @@
 <template>
-  <footer class="footer">
+  <footer class="footer" :class="route.path === '/results' ? 'footer__black' : ''">
     <div class="footer__wrapper">
       <div class="footer__description">
-        <h1 class="description__title">Санкт-Петербург, ул. Садовая 35 <span>Клиника GRANDMED</span></h1>
+        <h1 class="description__title" :class="route.path === '/results' ? 'title__white' : ''">Санкт-Петербург, ул. Садовая 35 <span>Клиника GRANDMED</span></h1>
         <div class="description__lists-wrapper">
           <ul class="description__list">
-            <li><NuxtLink to="mailto:oleg@gordienko.doctor">oleg@gordienko.doctor</NuxtLink></li>
-            <li><NuxtLink to="tel:79111224888">+7 911 122 48 88</NuxtLink></li>
-            <li><NuxtLink to="https://wa.me/79111224888" target="_blank">whatsapp</NuxtLink></li>
-            <li><NuxtLink to="https://t.me/doctor_gordienko" target="_blank">telegram</NuxtLink></li>
+            <li><NuxtLink to="mailto:oleg@gordienko.doctor" :class="route.path === '/results' ? 'title__grey' : ''">oleg@gordienko.doctor</NuxtLink></li>
+            <li><NuxtLink to="tel:79111224888" :class="route.path === '/results' ? 'title__grey' : ''">+7 911 122 48 88</NuxtLink></li>
+            <li><NuxtLink to="https://wa.me/79111224888" target="_blank" :class="route.path === '/results' ? 'title__grey' : ''">whatsapp</NuxtLink></li>
+            <li><NuxtLink to="https://t.me/doctor_gordienko" target="_blank" :class="route.path === '/results' ? 'title__grey' : ''">telegram</NuxtLink></li>
           </ul>
           <ul class="description__list">
-            <li><NuxtLink to="#">истории преображений</NuxtLink></li>
-            <li><NuxtLink to="#">вопросы и ответы</NuxtLink></li>
-            <li><NuxtLink to="profile">доктор гордиенко</NuxtLink></li>
-            <li><NuxtLink to="blog">блог</NuxtLink></li>
+            <li><NuxtLink to="#" :class="route.path === '/results' ? 'title__grey' : ''">истории преображений</NuxtLink></li>
+            <li><NuxtLink to="#" :class="route.path === '/results' ? 'title__grey' : ''">вопросы и ответы</NuxtLink></li>
+            <li><NuxtLink to="profile" :class="route.path === '/results' ? 'title__grey' : ''">доктор гордиенко</NuxtLink></li>
+            <li><NuxtLink to="blog" :class="route.path === '/results' ? 'title__grey' : ''">блог</NuxtLink></li>
           </ul>
           <ul class="description__list">
-            <li><NuxtLink to="#">Deep Plane Facelift</NuxtLink></li>
-            <li><NuxtLink to="#">лоб и брови</NuxtLink></li>
-            <li><NuxtLink to="#">глаза</NuxtLink></li>
-            <li><NuxtLink to="#">губы</NuxtLink></li>
-            <li><NuxtLink to="#">шея</NuxtLink></li>
+            <li><NuxtLink to="#" :class="route.path === '/results' ? 'title__grey' : ''">Deep Plane Facelift</NuxtLink></li>
+            <li><NuxtLink to="#" :class="route.path === '/results' ? 'title__grey' : ''">лоб и брови</NuxtLink></li>
+            <li><NuxtLink to="#" :class="route.path === '/results' ? 'title__grey' : ''">глаза</NuxtLink></li>
+            <li><NuxtLink to="#" :class="route.path === '/results' ? 'title__grey' : ''">губы</NuxtLink></li>
+            <li><NuxtLink to="#" :class="route.path === '/results' ? 'title__grey' : ''">шея</NuxtLink></li>
           </ul>
           <ul class="description__list">
-            <li><NuxtLink to="https://m.vk.com/ovgordienko?reactions_opened=wall-211145946_637" target="_blank">вконтакте</NuxtLink></li>
-            <li><NuxtLink to="https://www.instagram.com/gordienko.doctor?igsh=MThscGVwMXJ3NXVxaw==" target="_blank">instagram</NuxtLink></li>
-            <li><NuxtLink to="https://dzen.ru/oleggordienko" target="_blank">дзен</NuxtLink></li>
+            <li><NuxtLink to="https://m.vk.com/ovgordienko?reactions_opened=wall-211145946_637" target="_blank" :class="route.path === '/results' ? 'title__grey' : ''">вконтакте</NuxtLink></li>
+            <li><NuxtLink to="https://www.instagram.com/gordienko.doctor?igsh=MThscGVwMXJ3NXVxaw==" target="_blank" :class="route.path === '/results' ? 'title__grey' : ''">instagram</NuxtLink></li>
+            <li><NuxtLink to="https://dzen.ru/oleggordienko" target="_blank" :class="route.path === '/results' ? 'title__grey' : ''">дзен</NuxtLink></li>
           </ul>
         </div>
-        <img src="/static/assets/images/logo-grey.png" class="footer__logo">
+        <img src="/static/assets/images/logo-white.png" class="footer__logo" v-if="route.path === '/results'">
+        <img src="/static/assets/images/logo-grey.png" class="footer__logo" v-if="route.path !== '/results'">
       </div>
       <div class="footer__description">
         <img src="/static/assets/images/photo-blog.png">
-        <h1 class="description__blog-title">Блог</h1>
+        <h1 class="description__blog-title" :class="route.path === '/results' ? 'title__white' : ''">Блог</h1>
       </div>
     </div>
     <nav class="footer__links">
@@ -46,8 +47,10 @@
 
 <script setup>
 import { ref } from "vue";
+import { useRoute } from "vue-router";
 
 const currentYear = ref(new Date().getFullYear())
+const route = useRoute()
 </script>
 
 <style lang="scss" scoped>
@@ -58,6 +61,8 @@ const currentYear = ref(new Date().getFullYear())
   padding: 250px 0 17px 40px;
   max-width: 1728px;
   width: 100%;
+
+  &.footer__black {background: #000;}
 
   .footer__wrapper {
     display: flex;
@@ -81,6 +86,8 @@ const currentYear = ref(new Date().getFullYear())
         letter-spacing: 3px;
         text-transform: uppercase;
 
+        &.title__white {color: #fff;}
+
         span {
           color: #A3A3A3;
           font-size: 16px;
@@ -101,6 +108,8 @@ const currentYear = ref(new Date().getFullYear())
         line-height: 98%;
         text-transform: uppercase;
         margin-top: 50px;
+
+        &.title__white {color: #fff;}
       }
 
       .description__lists-wrapper {
@@ -124,6 +133,8 @@ const currentYear = ref(new Date().getFullYear())
             line-height: 95%;
             letter-spacing: 3px;
             text-transform: uppercase;
+
+            &.title__grey {color: #A3A3A3;}
           }
         }
       }
