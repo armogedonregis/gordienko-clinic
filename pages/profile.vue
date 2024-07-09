510b6f1442915f744049d83f43cec50b8ea58ba9
denosaur98 <denischurchill1998@gmail.com>
2024-07-09 20:03:05 +0300

    Revert "video autoplay"
    This reverts commit 534616bc4242456acafb38ae0b71973fb9a27ad0.

--- pages/profile.vue
+++ pages/profile.vue
@@ -1894,9 +1894,6 @@ function stopTypingAnimation(index) {
 function createObserver(callback, options) {
   return new IntersectionObserver(callback, options)
 }
-function preventFullscreen(event) {
-  event.preventDefault()
-}
 
 onMounted(() => {
   const subtitleOptions = {
@@ -1951,17 +1948,6 @@ onMounted(() => {
       }
       observerMap[className].observe(el)
     })
-  })
-
-  const videos = document.querySelectorAll('video')
-  videos.forEach((video, index) => {
-    if (index !== 0) {
-      video.removeAttribute('controls')
-      video.autoplay = true
-      video.load()
-      video.play()
-      video.addEventListener('click', preventFullscreen)
-    }
   })
 })
 onUnmounted(() => {
